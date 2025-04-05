ed217b96e8b3ff0e2db2e1dc8d18afe8edd54da4
Denisasal <137218365+Denisasal@users.noreply.github.com>
2025-04-05 16:49:15 +0200

    edit the arrow js script

--- Netflix_website/js_scripts/arrow_icon.js
+++ Netflix_website/js_scripts/arrow_icon.js
@@ -3,14 +3,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   var icon = document.querySelector(".icon");
   if (!icon) return;
-  // Show the icon after scrolling down 200px
+
   window.addEventListener("scroll", function () {
+    // Check scroll position
     if (window.scrollY > 200) {
-      icon.classList.add("visible");
+      icon.classList.add("visible"); // Add the "visible" class to show the icon
     } else {
-      icon.classList.remove("visible");
+      icon.classList.remove("visible"); // Remove the "visible" class to hide it
     }
   });
+
   // Scroll to the top when clicking the icon
   icon.addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
